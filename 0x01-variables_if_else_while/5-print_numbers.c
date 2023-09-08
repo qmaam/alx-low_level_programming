6e0bd17a073cf2a45e4c34927d6737cac3d558de
myusername <myemail@gmail.com>
2023-09-08 15:23:36 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: Print numbers from 0 to 9
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
