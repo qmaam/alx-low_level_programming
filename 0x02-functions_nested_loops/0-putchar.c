08c39a816ebd7b24c922f644b1df5b7f5a645385
myusername <myemail@gmail.com>
2023-09-12 14:54:28 -0700

    0 putcher

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 /**
  * main -Entry
  *
@@ -8,6 +8,15 @@
  */
 int main(void)
 {
-printf("_putchar\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+        _putchar('r');
+	_putchar('\n');
+        
+	return (0);
 }
