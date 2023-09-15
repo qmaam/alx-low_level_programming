dc8842e02e4b0abad76a051007cb12e2d7eaf9ab
myusername <myemail@gmail.com>
2023-09-15 03:01:27 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * isupper - Check if a letter 
- * number
- * Return
+ * isupper - Check if a letter is upper  
+ *@c: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
  */
 
 int _isupper(int c)
 
 {
-	if (c >= 65 && c <= 98)
-	{
-	return (1);	
-	}
-	return (0);
+	if (c >= 65 && c <= 90)
+		return (1);	
+	else
+		return (0);
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _putchar(char c);
-
 int _isupper(int c);
-
 int _isdigit(int c);
-
 int mul(int a, int b);
-
 void print_numbers(void);
-
 void print_most_numbers(void);
-
 void more_numbers(void);
-
 void print_line(int n);
+
+#endif
