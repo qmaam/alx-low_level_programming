595b0e8bde5e27ba73c1d1296b7f7842188d0799
myusername <myemail@gmail.com>
2023-09-18 12:07:24 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * 0-reset_to_98 -Takes a pointer to an int and updates
+ * 0-reset_to_98 - Takes a pointer to an int and updates
  * the value it points to 98
  *
- * @n: An integer input pointer
+ * @n: the value of a deferencing pointer
  *
  * Return : nothing
  */
